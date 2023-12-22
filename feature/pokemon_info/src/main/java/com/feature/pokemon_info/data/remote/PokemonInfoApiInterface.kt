750b44a92d53f2f28798e7773050c6b21f3d0eeb
festuskiambi <kiamfestoh@gmail.com>
2023-12-22 09:28:19 +0300

    Add get pokemon info api interface.

--- feature/pokemon_info/src/main/java/com/feature/pokemon_info/data/remote/PokemonInfoApiInterface.kt
+++ feature/pokemon_info/src/main/java/com/feature/pokemon_info/data/remote/PokemonInfoApiInterface.kt
@@ -1,4 +1,12 @@
 package com.feature.pokemon_info.data.remote
 
+import com.feature.pokemon_info.data.remote.model.GetPokemonInfoResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface PokemonInfoApiInterface {
+    @GET("pokemon/{id}/")
+    suspend fun getPokeMon(
+        @Path("id") id: String?,
+    ): GetPokemonInfoResponse
 }
